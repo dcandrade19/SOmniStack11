47446b3a4cf7c1fe513f569bc70a638caeb4dfde
dcandrade19 <dcandrade19@gmail.com.br>
2020-04-25 13:56:37 -0300

    Add testes

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
 const connection = require('../database/connection');
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 
 module.exports = {
     async index(req, res) {
@@ -17,7 +17,7 @@ module.exports = {
             uf
         } = req.body
     
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
     
         await connection('ongs').insert({
             id,
